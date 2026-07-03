17fbee401dd25c274309a83455ae8c182a42673c
Radha Malde <rmalde@umich.edu>
2026-07-02 21:28:21 -0400

    Portfolio updates

--- backtester/broker.py
+++ backtester/broker.py
@@ -1,1 +1,2 @@
 # Acts as a mediator between the backtesting engine and the portfolio. It handles the execution of trades, and updates the portfolio accordingly.
+# Implement later on.
--- backtester/portfolio.py
+++ backtester/portfolio.py
@@ -1,1 +1,17 @@
-# Contains the portfolio of the trader, including the cash, position, etc. 
+# Contains the portfolio of the trader, including the cash, position, etc. Updates according to engine
+
+class Portfolio():
+    def __init__(self, initial_cash):
+        self.cash = initial_cash
+        self.shares = 0
+
+    def execute_trade(self, signal, price):
+        # buy/sell as many as possible shares for now
+        if signal == "BUY":
+            shares_to_buy = self.cash//price
+                self.cash -= shares_to_buy*price
+                self.shares += shares_to_buy
+        elif signal == "SELL":
+                self.cash += self.shares * price
+                self.shares = 0
+
