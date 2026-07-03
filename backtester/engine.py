3bb6f94238ddca7b4510b3a623a12de8b6ddf516
Radha Malde <rmalde@umich.edu>
2026-07-03 15:04:23 -0400

    fixed updating data frame every day-- just need to update at start

--- backtester/engine.py
+++ backtester/engine.py
@@ -9,7 +9,8 @@ class Engine():
     def run_backtest(self): # runs day by day using the given data and strategy
         prepped_data = self.strategy.prep_data(self.data) # creates necessary columns for particular strategy
         for row in prepped_data:
-            signal = self.strategy.generate_signal(row, prepped_data)
+            uptodf = prepped_data.iloc[:row+1, :] # selects rows up to row+1, and all columns
+            signal = self.strategy.generate_signal(row, uptodf)
 
             self.portfolio.execute_trade(signal, price)
             # calculate necessary items.. rolling averages, days before, etc. 
